27210dba576e45e811192e4c8148756b9fc3a2ec
mahmoudmohamed22 <mahmoudmohamed5555@yahoo.com>
2021-05-14 19:51:49 +0200

    Download video

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
 #video link ,you will be download 
 
-link="https://www.youtube.com/watch?v=G6avw6VuA6A"
+link="https://www.youtube.com/watch?v=Rv3kVxOhm_Q"
 
 #object  video for link youtube
 video=YouTube(link)
@@ -18,3 +18,17 @@ print("\n--------------------------------------")
 print(f"The video length is \n {video.length/60} minutes ")
 print("\n--------------------------------------")
 
+#print(video.streams)
+#streams is information video in youtube link
+#for stream in video.streams:
+#   print(stream)
+
+print("\n--------------------------------------")
+#for stream in video.streams.filter(progressive=True,res="720p"):
+#    print(stream)
+
+def finish():
+    print("Download done")
+
+video.streams.get_highest_resolution().download(output_path="/home/ubuntu/Downloads")
+video.register_on_complete_callback(finish())
